a9cf92bba2d4844ba615872a27ac935179a81bd1
Khoa Le <ldakhoa1308@gmail.com>
2023-03-15 14:21:19 +0700

    Setup project

---
+++ IntegrationApp/IntegrationApp/IntegrationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntegrationAppApp.swift
+//  IntegrationApp
+//
+//  Created by Khoa Le on 15/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct IntegrationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- Sources/SwiftjectiveC/SwiftjectiveC.swift
+++
@@ -1,6 +0,0 @@
-public struct SwiftjectiveC {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
