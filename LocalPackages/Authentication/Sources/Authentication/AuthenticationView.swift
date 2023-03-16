f30a9c5879e5b9a4ef8e9fc268a22aee3276acfb
Khoa Le <ldakhoa1308@gmail.com>
2023-03-16 17:29:02 +0700

    Update token

--- LocalPackages/Authentication/Sources/Authentication/AuthenticationView.swift
+++ LocalPackages/Authentication/Sources/Authentication/AuthenticationView.swift
@@ -26,7 +26,7 @@ public struct AuthenticationView: View {
             Button("Login") {
                 let token = accessToken
                 Task {
-                    await viewModel.loginWithToken("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")
+                    await viewModel.loginWithToken(token)
                 }
                 accessToken = ""
             }
